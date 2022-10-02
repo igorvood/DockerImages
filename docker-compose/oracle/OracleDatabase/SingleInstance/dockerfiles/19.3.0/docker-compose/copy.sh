3481f8ede9ebad4e07a994c7dcaa67fee639501a
vood <balitsky_igor@list.ru>
2022-10-02 14:51:08 +0300

    init

---
+++ docker-compose/oracle/OracleDatabase/SingleInstance/dockerfiles/19.3.0/docker-compose/copy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo cp -f /home/vood/IdeaProjects/DockerImages/docker-compose/oracle/OracleDatabase/SingleInstance/dockerfiles/19.3.0/docker-compose/Docker_Oracle.desktop /usr/share/applications/
---
+++ docker-compose/oracle/OracleDatabase/SingleInstance/dockerfiles/19.3.0/docker-compose/oracle_docker_compose.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /home/vood/IdeaProjects/DockerImages/docker-compose/oracle/OracleDatabase/SingleInstance/dockerfiles/19.3.0/docker-compose
+
+docker-compose up
