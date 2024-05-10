ebfeb62b7d64e1905fe010805e2dc061d6b7bcd5
vood <igor-vood@gmal.com>
2024-05-10 15:31:38 +0300

    linux

--- linux/install/ideaIUltimate-2023.3.2/idea.sh
+++ linux/install/ideaIUltimate-2023.3.2/idea.sh
@@ -6,9 +6,9 @@ echo "======================= #ideaIUltimate-2023.3.2 ======================="
 tar xvf ideaIU-2023.3.2.tar.gz
 sudo mv ./idea-IU-233.13135.103 /opt/ideaIUltimate-2023.3.2
 
-sudo cp /IdeaUltimate2023.3.2.desktop  $HOME/.local/share/applications
-sudo cp /idea.png  /opt/ideaIUltimate-2023.3.2
+sudo cp -f IdeaUltimate2023.3.2.desktop  $HOME/.local/share/applications
+sudo cp -f idea.png  /opt/ideaIUltimate-2023.3.2
 
 tar -xvf jetbra.tar.gz
-sudo mv ./jetbra /opt/ideaIUltimate-2023.3.2/jetbra
-sudo cp /idea64.vmoptions  /opt/ideaIUltimate-2023.3.2/bin/
+sudo mv -f ./jetbra /opt/ideaIUltimate-2023.3.2/jetbra
+sudo cp -f idea64.vmoptions  /opt/ideaIUltimate-2023.3.2/bin/
