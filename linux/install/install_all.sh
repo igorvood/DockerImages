dcb227a63c1f62946d10c887583a1eaa7fb7acad
vood <igor-vood@gmail.com>
2024-05-10 17:54:46 +0300

    linux

--- linux/install/install_all.sh
+++ linux/install/install_all.sh
@@ -2,15 +2,6 @@
 
 echo "сегодня " `date`
 
-#yandex
-/bin/bash ./yandex/install.sh > yandex.log
-
-#keyboard
-echo "======================= keyboard ======================="
-echo 2 | sudo tee /sys/module/hid_apple/parameters/fnmode
-echo options hid_apple fnmode=2 | sudo tee -a /etc/modprobe.d/hid_apple.conf
-sudo update-initramfs -u -k all
-
 
 echo "======================= openjdk-17 ======================="
 sudo apt update && sudo apt upgrade
---
+++ linux/install/openjdk-17/install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "======================= openjdk-17 ======================="
+sudo apt update && sudo apt upgrade
+sudo apt install openjdk-17-jdk openjdk-17-jre
