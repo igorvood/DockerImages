770eed057cd8aead2536bd9ef227d7e08fe735d3
vood <igor-vood@gmail.ru>
2024-05-10 19:30:12 +0300

    linux

--- linux/install/012_tor/install.sh
+++ linux/install/012_tor/install.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 echo "======================= tor ======================="
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-
-sudo apt update && sudo apt upgradejdk openjdk-17-jre
