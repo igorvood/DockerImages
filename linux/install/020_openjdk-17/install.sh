3e41ce16fd405d42e5c6602d39fa5e0d3b9414ff
vood <igor-vood@gmail.com>
2024-05-10 20:32:46 +0300

    linux

--- linux/install/020_openjdk-17/install.sh
+++ linux/install/020_openjdk-17/install.sh
@@ -2,3 +2,8 @@
 echo "======================= openjdk-17 ======================="
 sudo apt update && sudo apt upgrade
 sudo apt install openjdk-17-jdk openjdk-17-jre
+
+sudo cp java.sh /etc/profile.d/java.sh
+source /etc/profile.d/java.sh
+
+sudo chmod +x /etc/profile.d/java.sh
---
+++ linux/install/020_openjdk-17/java.sh
@@ -0,0 +1,2 @@
+export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
+export PATH=$PATH:/usr/lib/jvm/java-17-openjdk-amd64/bin
