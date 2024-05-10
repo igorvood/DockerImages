50e56aa052dff973d468b03802a9c2270198a2e9
vood <igor-vood@gmail.ru>
2024-05-10 19:39:03 +0300

    linux

--- linux/install/012_tor/install.sh
+++ linux/install/012_tor/install.sh
@@ -2,3 +2,4 @@
 echo "======================= tor ======================="
 sudo apt update && sudo apt upgrade
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub com.github.micahflee.torbrowser-launcher -y
--- linux/install/013_telegram/install.sh
+++ linux/install/013_telegram/install.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 echo "======================= telegram ======================="
+sudo apt update && sudo apt upgrade
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub org.telegram.desktop
