17eb43cea30cea65a697eec8234fba9c3018aed1
vood <igor-vood@gmal.com>
2024-05-10 13:15:14 +0300

    inst

---
+++ linux/install/ideaIUltimate-2023.3.2/idea.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#ideaIUltimate-2023.3.2
+echo "======================= #ideaIUltimate-2023.3.2 ======================="
+
+tar xvf ideaIU-2023.3.2_Lin.zip
+#sudo mv ./idea-IC-241.15989.150 /opt/ideaIC-2024.1.1
+
+#sudo cp ./IdeaComunity2024.1.1.desktop  $HOME/.local/share/applications
