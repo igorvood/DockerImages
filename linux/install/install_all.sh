8f29821e4b5c16ff343725ee082b5d87244c0a8a
vood <igor-vood@gmal.com>
2024-05-10 15:38:49 +0300

    linux

--- linux/install/install_all.sh
+++ linux/install/install_all.sh
@@ -2,6 +2,16 @@
 
 echo "сегодня " `date`
 
+
+#yandex
+sudo apt update && sudo apt upgrade
+sudo apt install dirmngr ca-certificates software-properties-common apt-transport-https curl -y
+curl -fsSL https://repo.yandex.ru/yandex-browser/YANDEX-BROWSER-KEY.GPG | gpg --dearmor | sudo tee /usr/share/keyrings/yandex.gpg > /dev/null
+echo deb [arch=amd64 signed-by=/usr/share/keyrings/yandex.gpg] http://repo.yandex.ru/yandex-browser/deb stable main | sudo tee /etc/apt/sources.list.d/yandex-stable.list
+sudo apt update
+sudo apt install yandex-browser-stable -y
+sudo rm /etc/apt/sources.list.d/yandex-browser*.list
+
 #keyboard
 echo "======================= keyboard ======================="
 echo 2 | sudo tee /sys/module/hid_apple/parameters/fnmode
