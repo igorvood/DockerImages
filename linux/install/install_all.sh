edbde3baa3674bf069dc272c901a0a1c1cd16440
vood <igor-vood@gmal.com>
2024-05-10 15:55:27 +0300

    linux

--- linux/install/install_all.sh
+++ linux/install/install_all.sh
@@ -2,15 +2,8 @@
 
 echo "сегодня " `date`
 
-
 #yandex
-sudo apt update && sudo apt upgrade
-sudo apt install dirmngr ca-certificates software-properties-common apt-transport-https curl -y
-curl -fsSL https://repo.yandex.ru/yandex-browser/YANDEX-BROWSER-KEY.GPG | gpg --dearmor | sudo tee /usr/share/keyrings/yandex.gpg > /dev/null
-echo deb [arch=amd64 signed-by=/usr/share/keyrings/yandex.gpg] http://repo.yandex.ru/yandex-browser/deb stable main | sudo tee /etc/apt/sources.list.d/yandex-stable.list
-sudo apt update
-sudo apt install yandex-browser-stable -y
-sudo rm /etc/apt/sources.list.d/yandex-browser*.list
+/bin/bash ./yandex/install.sh > yandex.log
 
 #keyboard
 echo "======================= keyboard ======================="
