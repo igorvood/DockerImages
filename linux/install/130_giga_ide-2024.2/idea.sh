3a761d95b80ef5ab1ce238823b678865848de833
vood <igor-vood@gmail.com>
2024-10-12 12:46:12 +0300

    linux

---
+++ linux/install/130_giga_ide-2024.2/idea.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#ideaIUltimate-2023.3.2
+tar -xzvf gigaideCE-242.21829.142.tar.gz
+#echo "======================= #ideaIUltimate-2023.3.2 ======================="
+#sudo mkdir /opt/ideaIUltimate-2023.3.2
+#sudo tar xvf ideaIU-2023.3.2.tar.gz -C /opt/ideaIUltimate-2023.3.2
+#sudo mkdir /opt/ideaIUltimate-2023.3.2/jetbra
+#sudo tar -xvf jetbra.tar.gz -C /opt/ideaIUltimate-2023.3.2/jetbra
+#
+sudo mv ./idea-IU-233.13135.103 /opt/ideaIUltimate-2023.3.2
+#sudo cp -f IdeaUltimate2023.3.2.desktop  /usr/share/applications
+#sudo cp -f idea.png  /opt/ideaIUltimate-2023.3.2
+#
+#
+sudo mv -f ./jetbra /opt/ideaIUltimate-2023.3.2/jetbra
+#cp -f idea64.vmoptions  /opt/ideaIUltimate-2023.3.2/idea-IU-233.13135.103/bin/
+#/opt/ideaIUltimate-2023.3.2/jetbra/jetbra/scripts/install.sh
