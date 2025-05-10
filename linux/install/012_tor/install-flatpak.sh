a9be33c66fe68b5eef2da067e85ea9c06825c147
tora <balitsky_igor@list.ru>
2025-05-10 11:47:23 +0300

    q

--- linux/install/012_tor/install-flatpak.sh
+++ linux/install/012_tor/install-flatpak.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "======================= tor ======================="
+sudo mv /etc/apt/preferences.d/nosnap.pref /etc/apt/preferences.d/nosnap.pref.backup
+
 sudo apt update && sudo apt upgrade
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub com.github.micahflee.torbrowser-launcher -y
