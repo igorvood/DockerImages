a78a3481c4dc329683f27fd53bcbf38f88c6e2f2
vood <igor-vood@gmail.com>
2025-05-10 18:25:18 +0300

    linux

---
+++ linux/install/040_gradle/8.12/gradle.sh
@@ -0,0 +1,2 @@
+export GRADLE_HOME=/opt/gradle/gradle-8.12
+export PATH=$PATH:/opt/gradle/gradle-8.12/bin
---
+++ linux/install/040_gradle/8.12/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt update && sudo apt upgrade
+sudo apt install unzip
+sudo wget https://services.gradle.org/distributions/gradle-8.12-bin.zip
+sudo unzip -d /opt/gradle gradle-8.12-bin.zip
+
+sudo cp gradle.sh /etc/profile.d/gradle.sh
+source /etc/profile.d/gradle.sh
+
+sudo chmod +x /etc/profile.d/gradle.sh
+
