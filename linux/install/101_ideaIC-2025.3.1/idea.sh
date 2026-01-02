69aa36ba135e43886bb1bed16602f867f12fbd1c
vood <balitsky_igor@list.ru>
2026-01-02 11:57:58 +0300

    idea

---
+++ linux/install/101_ideaIC-2025.3.1/idea.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#ideaIC-2022.2.1
+echo "======================= ideaIC-2025.3.1 ======================="
+# sudo mkdir /opt/ideaIC-2025.3.1
+# sudo tar xvf idea-2025.3.1.tar.gz -C /opt/ideaIC-2025.3.1
+
+sudo yes | cp -rf ./IdeaComunity2025.3.1.desktop  /usr/share/applications
+sudo yes | cp -rf ./idea.png  /opt/ideaIC-2025.3.1
